367e69752b2db0220a85a4e25fd9a53608a3aa19
Ashok Kumar <ashokkumar07116@gmail.com>
2026-07-17 21:25:56 +0530

    latest version added

--- lambda_function.py
+++ lambda_function.py
@@ -5,6 +5,6 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "Lambda deployed automatically from GitHub!"
+            "message": "Lambda deployed automatically from GitHub V1!"
         })
     }
